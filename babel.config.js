5fd0a32ede7a9c2dcf5f65cc49e53f419bca191a
Umut Uğur <umutugur@Umut-MacBook-Air.local>
2026-02-21 23:53:35 +0300

    fix: metro alias for @core in monorepo

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+          alias: {
+            "@core": "./apps/mobile/src/core",
+            "@features": "./apps/mobile/src/features",
+            "@ui": "./apps/mobile/src/ui",
+            "@assets": "./apps/mobile/assets",
+          },
+        },
+      ],
+    ],
+  };
+};
